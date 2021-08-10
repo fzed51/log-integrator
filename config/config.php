085667db64e9f6c99b1b3bf86e22cdbba6f55233
Fabien Sanchez <fzed51+git@gmail.com>
2021-08-10 04:52:57 +0200

    feat / mise en place du logger

--- config/config.php
+++ config/config.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 return [
     "log" => [
         "directory" => "logs",
-        "chanel" => "log-integrator"
+        "channel" => "log-integrator"
     ]
 ];
--- src/dependencies.php
+++ src/dependencies.php
@@ -1,6 +1,28 @@
 <?php
 declare(strict_types=1);
 
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Logger;
+use Monolog\Processor\IntrospectionProcessor;
+use Monolog\Processor\UidProcessor;
+use Monolog\Processor\WebProcessor;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
 use Slim\App;
 
-return static function (App $app): void {};
+return static function (App $app): void {
+    $c = $app->getContainer();
+
+    $c[LoggerInterface::class] = static function (ContainerInterface $c): LoggerInterface {
+        $config = $c->get('settings')['log'];
+        $logger = new Logger($config['channel']);
+        $logger->pushHandler(new RotatingFileHandler(
+            'log', 10, Logger::DEBUG
+        ));
+        $logger
+            ->pushProcessor(new UidProcessor())
+            ->pushProcessor(new IntrospectionProcessor())
+            ->pushProcessor(new WebProcessor());
+        return $logger;
+    };
+};
